e6b3845a42e8f53676e1a4732ce44c41228e4112
ZetyrStudy <97560331+ZetyrStudy@users.noreply.github.com>
2022-03-23 21:05:16 -0300

    20220323_01

--- conn.php
+++ conn.php
@@ -1,25 +1,39 @@
 <?php
 
-// 1) PHP em UTF-8 - Primeiro código PHP
+/**
+ * Passo 1) PHP em UTF-8
+ * Essa deve ser SEMPRE a primeira linha de código PHP.
+ */
 header("Content-type: text/html; charset=utf-8");
 
-// 2) Conexão com o banco de dados
-
-// Variáveis de conexão
+/**
+ * Passo 2) Variáveis de conexão
+ * Os dados destas variáveis devem ser obtidos com o provedor de hospedagem.
+ * Os dados abaixo representam uma conexão usando o provedor XAMPP.
+ */
 $servidor = 'localhost';
 $usuario = 'root';
 $senha = '';
-$banco = 'review';
+$banco = 'sitephp';
 
-// Conexão
+/**
+ * Passo 3) Conexão com o MySQL usando a biblioteca 'mysqli' do PHP.
+ * Esa biblioteca já vem instalada, por padrão, no PHP.
+ */
 $conn = new mysqli($servidor, $usuario, $senha, $banco);
 
-// 3) Forçar todas as transações entre PHP e MySQL para UTF-8
+/**
+ * Passo 4) Forçar todas as transações entre PHP e MySQL para UTF-8
+ * Fazemos isso para evitar erros de acentuação quando lemos ou gravamos dados no MySQL.
+ */
 $conn->query("SET NAMES 'utf8'");
 $conn->query('SET character_set_connection=utf8');
 $conn->query('SET character_set_client=utf8');
 $conn->query('SET character_set_results=utf8');
 
-// 4) Forçar nomes em datas para pt-BR (dias da semana e meses)
+/**
+ * Passo 5) Forçar nomes em datas para pt-BR (dias da semana e meses)
+ * Isso é útil para sites em português. Para outros idiomas, troque a sigla do idioma.
+ */
 $conn->query('SET GLOBAL lc_time_names = pt_BR');
 $conn->query('SET lc_time_names = pt_BR');
